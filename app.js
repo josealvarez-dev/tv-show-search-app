404cc6aaaaca87effea447126d466626534304ce
josealvarez-dev <josemanuelalvarezmamani@hotmail.com>
2025-12-06 23:22:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,14 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
 
-    // Obtener texto
     const searchTerm = form.elements.query.value;
 
-    // Configurar búsqueda
     const config = { params: { q: searchTerm } }
 
-    // Pedir datos
     const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
 
-    // Mostrar imágenes
     makeImages(res.data);
 
-    // Limpiar input
     form.elements.query.value = '';
 });
 
@@ -27,4 +22,5 @@ const makeImages = (shows) => {
             document.body.append(img);
         }
     }
+
 }
